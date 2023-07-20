326383f64bf8c349060dbee18e6968080a9123a9
EshaAfzal07 <135261194+EshaAfzal07@users.noreply.github.com>
2023-07-19 18:25:01 -0700

    shipping.py

--- shipping.py
+++ shipping.py
@@ -1,43 +1,40 @@
+import json
+import random
 import sqlite3
-conn = sqlite3.connect('shipping.db')
-cursor = conn.cursor()
 
-
-cursor.execute('''CREATE TABLE IF NOT EXISTS shipments (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    destination TEXT NOT NULL,
-                    weight REAL NOT NULL,
-                    shipping_cost REAL NOT NULL
-                  )''')
-
-conn.commit()
-conn.close()
-
+def create_database():
+    db = sqlite3.connect("shipping_details.db")
+    cursor = db.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS shipping_records (id INTEGER PRIMARY KEY, destination TEXT, weight REAL, shipping_cost REAL)")
+    db.commit()
+    return db
 
 def calculate_shipping_cost(destination, weight):
     rate_per_kg = 5.0
     return weight * rate_per_kg
 
+def save_shipping_details(destination, weight, shipping_cost, shipping_data):
+    shipping_id = random.randint(1, 1000)
+    shipping_data["id"] = shipping_id
 
-def save_shipping_details(destination, weight, shipping_cost):
-    # Save the shipping details in the database
-    conn = sqlite3.connect('shipping.db')
+    conn = sqlite3.connect('shipping_details.db')
     cursor = conn.cursor()
-    cursor.execute('INSERT INTO shipments (destination, weight, shipping_cost) VALUES (?, ?, ?)',
-                   (destination, weight, shipping_cost))
+    cursor.execute('INSERT INTO shipping_records (id, destination, weight, shipping_cost) VALUES (?, ?, ?, ?)',
+                   (shipping_id, shipping_data["destination"], shipping_data["weight"], shipping_cost))
     conn.commit()
     conn.close()
+    return shipping_id
 
+def main(destination, weight):
+    shipping_cost = calculate_shipping_cost(destination, weight)
+    shipping_data = {"destination": destination, "weight": weight}
+    shipping_id = save_shipping_details(destination, weight, shipping_cost, shipping_data)
+    print(f"Shipping details saved. ID: {shipping_id}")
 
 if __name__ == '__main__':
-    destination = 'New York'
+    destination = "New York"
     weight = 2.5
+    main(destination, weight)
 
-    shipping_cost = calculate_shipping_cost(destination, weight)
-    print(f'Shipping cost to {destination} for {weight} kg package: {shipping_cost}')
 
-    save_shipping_details(destination, weight, shipping_cost)
-    print('Shipping details saved to the database.')
-OUTPUT:
-Shipping cost to New York for 2.5 kg package: 12.5
-Shipping details saved to the database.
+
